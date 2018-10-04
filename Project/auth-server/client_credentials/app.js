12024e03c743000a0878a8428ac77231011884a4
Gisellad <giselladieguez@yahoo.com.ar>
2018-10-04 00:25:46 -0300

    Final project

---
+++ Project/auth-server/client_credentials/app.js
@@ -0,0 +1,30 @@
+var request = require('request');
+var client_id = '205f8524aefa4767b564ebb77ff64b78';
+var client_secret = 'b68e98c7668b4a5c90888ec482ffbe6a';
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/users/jmperezperez',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function(error, response, body) {
+      console.log(body);
+    });
+  }
+});
